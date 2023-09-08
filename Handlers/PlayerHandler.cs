6adf60587a0b4eaad9131067ab994a00aa4ced5b
JustMarfix <justmarfix@vk.com>
2023-09-08 11:59:01 +0300

    krill issue x2

--- Handlers/PlayerHandler.cs
+++ Handlers/PlayerHandler.cs
@@ -23,10 +23,7 @@ namespace BetterDoorAccess.Handlers
                     {
                         if (ev.Player.CurrentItem.Type == pair.Key)
                         {
-                            if (!pair.Value)
-                            {
-                                ev.IsAllowed = false;
-                            }
+                            ev.IsAllowed = pair.Value;
                         }
                     }
                 }
